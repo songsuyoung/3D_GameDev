3cdf16b2c852654fe3626148622eaac9a1fbeb38
songsuyoung <tndud3999@naver.com>
2024-01-12 17:19:31 +0900

    플레이어 회전 버그 수정

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,14 +8,12 @@ public class PlayerController : MonoBehaviour
 
     Transform player;
     Animator playerAnimator;
-    Rigidbody playerRigidbody;
     [SerializeField]
-    float speed = 10f;
+    float speed;
     [SerializeField]
     float jumpHeight = 3f;
-    [SerializeField]
-    float rotateSpeed = 5f;
-    Vector3 dir= Vector3.zero;
+
+    Vector3 dir;
 
     [SerializeField]
     Transform playerCamera;
@@ -23,7 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player").transform;
         playerAnimator = player.GetComponent<Animator>();
-        playerRigidbody= player.GetComponent<Rigidbody>();
+        dir = Vector3.zero;
     }
 
     private void FixedUpdate()
@@ -42,17 +40,17 @@ public class PlayerController : MonoBehaviour
         Vector3 cameraForward = playerCamera.forward; //카메라가 바라보는 앞 방향
         Vector3 cameraRight = playerCamera.right; //원하는 방향(즉 (x,0,0) + (0,0,z) 정규화 진행)
         Vector3 moveDirection = (cameraForward * dir.z + cameraRight * dir.x).normalized;
-
-        // 이동
-        player.transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+        moveDirection.y = 0; //y축회전을 수행하기 위해서 강제로 0으로 초기화해준다.
 
         // 카메라와 캐릭터 사이의 간격에 대해 회전
         if (moveDirection != Vector3.zero)
         {
-            //회전에 대해 다시생각해
-
+            player.forward = Vector3.Slerp(player.forward, moveDirection, speed*Time.deltaTime);
         }
-       
+
+        // 이동
+        player.transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+
         float offset = 0.5f + Input.GetAxis("Sprint") * 0.5f; //Shift 누를 시 스피드 추가 예정
 
         playerAnimator.SetFloat("Horizontal", moveDirection.x * offset);
