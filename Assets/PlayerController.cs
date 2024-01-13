bec215931ea1ecff3a950be83117f8ea18baddde
songsuyoung <tndud3999@naver.com>
2024-01-13 20:39:54 +0900

    Player 속도 변화 -> TransformDirection함수를 이용해서 월드 좌표를 가져와 해결

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : MonoBehaviour
@@ -8,57 +11,64 @@ public class PlayerController : MonoBehaviour
 
     Transform player;
     Animator playerAnimator;
+    //캐릭터 컨트롤러의 경우, 중력이 적용되지 않음. 그래서 직접적으로 중력을 적용
+    CharacterController characterController;
     [SerializeField]
     float speed;
     [SerializeField]
     float jumpHeight = 3f;
 
     Vector3 dir;
-
+    private float gravity = -9.81f;//중력계수 
     [SerializeField]
     Transform playerCamera;
     private void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
         playerAnimator = player.GetComponent<Animator>();
+        characterController = player.GetComponent<CharacterController>();
         dir = Vector3.zero;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Move();
     }
+
     void Move()
-    {
-
-        //가로로 A-D
-        dir.x = Input.GetAxis("Horizontal");
+    {   //가로로 A-D
+        dir.x = Input.GetAxisRaw("Horizontal");
         //세로로, W-S키
-        dir.z = Input.GetAxis("Vertical");
-        //카메라가 바라보는 방향으로 플레이어 방향을 이동시킨다.
-        // 카메라의 forward 방향을 기반으로 이동 방향 계산
-        Vector3 cameraForward = playerCamera.forward; //카메라가 바라보는 앞 방향
-        Vector3 cameraRight = playerCamera.right; //원하는 방향(즉 (x,0,0) + (0,0,z) 정규화 진행)
-        Vector3 moveDirection = (cameraForward * dir.z + cameraRight * dir.x).normalized;
-        moveDirection.y = 0; //y축회전을 수행하기 위해서 강제로 0으로 초기화해준다.
+        dir.z = Input.GetAxisRaw("Vertical");
+        // 카메라가 바라보는 방향으로 플레이어 방향을 이동시킨다.
 
-        // 카메라와 캐릭터 사이의 간격에 대해 회전
-        if (moveDirection != Vector3.zero)
+        Vector3 forward = player.TransformDirection(Vector3.forward); //플레이어의 현재 z좌표를 월드에서
+        Vector3 right = player.TransformDirection(Vector3.right); //플레이어의 현재 x좌표(월드)
+        
+        Vector3 moveDirection = (forward * dir.z + right * dir.x).normalized;
+
+        if(characterController.isGrounded==false)
         {
-            player.forward = Vector3.Slerp(player.forward, moveDirection, speed*Time.deltaTime);
+            moveDirection.y += gravity * Time.deltaTime;
         }
-
         // 이동
-        player.transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+        characterController.Move(moveDirection * speed * Time.deltaTime);
 
-        float offset = 0.5f + Input.GetAxis("Sprint") * 0.5f; //Shift 누를 시 스피드 추가 예정
+        float offset = 0.5f + Input.GetAxis("Sprint") * 0.5f; // Shift 누를 시 스피드 추가 예정
 
         playerAnimator.SetFloat("Horizontal", moveDirection.x * offset);
         playerAnimator.SetFloat("Vertical", moveDirection.z * offset);
 
-        //Jump 추가예정
-    }
+        Debug.Log(moveDirection);
 
+        // 회전
+        if (moveDirection != Vector3.zero)
+        {
+            Vector3 toRotation = Vector3.Scale(playerCamera.forward, new Vector3(1f, 0, 1f));
+            player.rotation = Quaternion.Slerp(player.rotation, Quaternion.LookRotation(toRotation), speed * Time.deltaTime);
+        }
+
+    }
 
     void Attack()
     {
